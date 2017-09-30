b994fca8c011fd7816010047b3d38750fbaf049a
David Carboni <david@carboni.io>
2017-09-30 12:27:56 +0100

    Responding to the Slack challenge, hopefully.

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
-from flask import Flask
+import logging
+from flask import Flask, request
 
 PORT = int(os.getenv("PORT", "5000"))
 
 app = Flask("lifeinteal-bot")
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
     return "ohai"
+
+
+@app.route('/', methods=['POST'])
+def challenge():
+    log.debug("Form: " + str(request.form))
+    return request.form.get("challenge") or "wat"
 
 
 if __name__ == '__main__':
