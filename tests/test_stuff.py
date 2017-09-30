4c52c878ba7642e9b55ad5d1f00fc3b5da4fd06d
David Carboni <david@carboni.io>
2017-09-30 11:54:56 +0100

    Better testing of tests.

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -1,1 +1,10 @@
-print("ohai!")
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_nothing(self):
+        print("ohai")
+
+
+if __name__ == '__main__':
+    unittest.main()
